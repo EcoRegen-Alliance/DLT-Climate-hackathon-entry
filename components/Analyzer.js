f252ac27b6533f6eb564346bff06aa76cd73aebf
Tolu john <104234518+Tolujoh-n@users.noreply.github.com>
2024-04-04 17:01:19 +0100

    figma ui completed

--- components/Analyzer.js
+++ components/Analyzer.js
@@ -22,10 +22,11 @@ function Analyzer() {
             <div className="col-12">
               <div className="card info-card customers-card">
                 <div className="card-body">
-                  <h5 className="card-title">ECO Token Analyzer</h5>
+                  <br></br>
+                  {/* <h5 className="card-title">ECO Token Analyzer</h5>
                   <div id="content">
                     <p>Pay for real-time satellite analysis</p>
-                  </div>
+                  </div> */}
                   <div className="col-12">
                     <Map />
                   </div>
@@ -40,11 +41,45 @@ function Analyzer() {
                                 style={{ padding: "10px" }}
                                 className="info-box card"
                               >
-                                <h3>Verification Fees Balance</h3>
-                                <p>$0.00</p>
-                                <p>
-                                  List of the last payment to ACME Verifiers
-                                </p>
+                                <div className="d-flex justify-content-between align-items-center">
+                                  {/* First word with icon */}
+                                  <div style={{ marginRight: "10px" }}>
+                                    <span>
+                                      <p>Created:</p>
+                                      <p>Location:</p>
+                                      <p>Carbon in situ</p>
+                                    </span>
+                                  </div>
+                                  <div>
+                                    <span>
+                                      <p>1 Minutes ago</p>
+                                      <p>0x4a30</p>
+                                      <p>Yes</p>
+                                    </span>
+                                  </div>
+                                </div>
+                              </div>
+                              <div
+                                style={{ padding: "10px" }}
+                                className="info-box card"
+                              >
+                                <div className="d-flex justify-content-between align-items-center">
+                                  {/* First word with icon */}
+                                  <div style={{ marginRight: "10px" }}>
+                                    <span>
+                                      <p>Transferred:</p>
+                                      <p>From:</p>
+                                      <p>To</p>
+                                    </span>
+                                  </div>
+                                  <div>
+                                    <span>
+                                      <p>1 Minutes ago</p>
+                                      <p>0x4a30</p>
+                                      <p>0x4a30</p>
+                                    </span>
+                                  </div>
+                                </div>
                               </div>
                             </div>
                           </div>
@@ -57,9 +92,71 @@ function Analyzer() {
                             style={{ padding: "10px" }}
                             className="info-box card"
                           >
-                            <h3>Profit Per Verification Token</h3>
-                            <p>$0.00</p>
-                            <p>To date total</p>
+                            <div className="d-flex justify-content-between align-items-center">
+                              {/* First word with icon */}
+                              <div style={{ marginRight: "10px" }}>
+                                <span>
+                                  <button className="btn btn-primary">
+                                    Real Time
+                                  </button>
+                                </span>
+                              </div>
+                              <div>
+                                <span>
+                                  <button className="btn btn-primary">
+                                    Pay for analysis
+                                  </button>
+                                </span>
+                              </div>
+                            </div>
+                            <div className="realtime">
+                              <br></br>
+
+                              <div style={{ marginRight: "10px" }}>
+                                <h3>Pay for real-time satellite analysis</h3>
+                              </div>
+                              <div className="d-flex justify-content-between align-items-center">
+                                {/* First word with icon */}
+
+                                <div style={{ marginRight: "10px" }}>
+                                  <span>
+                                    <p>Price:</p>
+                                    <p>Carbon in situ:</p>
+                                  </span>
+                                </div>
+                                <div>
+                                  <span>
+                                    <p>0.1 Hbar</p>
+                                    <p>Yes</p>
+                                  </span>
+                                </div>
+                              </div>
+                              <button className="btn btn-primary">Pay</button>
+                            </div>
+                            <div className="realtime">
+                              <br></br>
+
+                              <div style={{ marginRight: "10px" }}>
+                                <h3>Data feed from hosting organization</h3>
+                              </div>
+                              <div className="d-flex justify-content-between align-items-center">
+                                {/* First word with icon */}
+
+                                <div style={{ marginRight: "10px" }}>
+                                  <span>
+                                    <p>Price:</p>
+                                    <p>Carbon in situ:</p>
+                                  </span>
+                                </div>
+                                <div>
+                                  <span>
+                                    <p>0.1 Hbar</p>
+                                    <p>Yes</p>
+                                  </span>
+                                </div>
+                              </div>
+                              <button className="btn btn-primary">Pay</button>
+                            </div>
                           </div>
                         </div>
                       </div>
